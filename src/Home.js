d219015d150718759bde7e92e1c499590447c05a
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-06-29 22:42:33 -0500

    Structure - built sidebar, modal and social icons

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { FaBars } from 'react-icons/fa';
+
+const Home = () => {
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn">show modal</button>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+const Modal = () => {
+  return (
+    <div className={`show-modal`}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

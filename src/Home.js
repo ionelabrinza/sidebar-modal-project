cdabde28fa09e3239c7731e4eb88d6d87084b099
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-07-01 23:18:39 -0500

    Complete

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,15 @@ import { FaBars } from 'react-icons/fa';
 import { AppContext, useGlobalContext } from './context';
 
 const Home = () => {
-  const data = useGlobalContext();
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
